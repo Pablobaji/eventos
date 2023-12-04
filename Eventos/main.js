3c4e97b5a4a5a964d46b3317c288c639231c80c7
Pablo <144966084+Pablobaji@users.noreply.github.com>
2023-12-04 12:56:34 -0600

    Add files via upload

---
+++ Eventos/main.js
@@ -0,0 +1,17 @@
+// let button = document.querySelector('.button');
+
+// function eventos() {
+//    button.addEventListener('mouseout', console.log('Estamos sobre el boton'));
+//    console.log('Salimos del boton', )
+// }
+// window.addEventListener('keydown',(e)=>{
+//     console.log('Pulso una tecla');
+//     console.log(String.fromCharCode(e.keyCode));
+// })
+// window.addEventListener('keyup',(e) =>{
+//     console.log('Deje de precionar la tecla')
+// }) 
+function texto() {
+    let.txt = document.getElementById('tx').value;
+    document.textContent
+}
